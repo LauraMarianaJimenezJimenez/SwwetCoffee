6e5a43e672154b459c5df2206fa5f756b4eb4e2f
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2021-02-28 17:47:58 -0500

    NavBar and Carousel done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { NavebarComponent } from './navebar/navebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    CarouselComponent,
+    NavebarComponent
   ],
   imports: [
     BrowserModule
