13a68a35d2d4ae5d17c628c484b32f741bfa60e9
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2021-03-01 17:26:49 -0500

    NgModel registro added

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  public nombre ="";
+  public apellido ="";
+  public email = "";
+  public contrasena = "";
+  public contrasena2 = "";
+  public celular=0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  registrarse(): void
+  {
+    alert(this.nombre + " " + this.apellido + " " + this.celular + 
+    " " + this.contrasena  + " " + this.contrasena2  + " " + this.email  + " " + this.celular)
   }
 
 }
