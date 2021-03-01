eb43633479d011a61b8e8e5a5141d1acbe98c5d1
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2021-03-01 09:35:29 -0500

    Template Catalogo done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { CarouselComponent } from "./carousel/carousel.component";
 import{LoginComponent} from "./login/login.component"
 import { NavebarComponent } from "./navebar/navebar.component";
 import { RegistroComponent } from "./registro/registro.component";
+import { TiendaComponent } from "./tienda/tienda.component";
 
 const routes = [
     {path: '', component: LoginComponent},
     {path:'registrarse',component: RegistroComponent},
-    {path:'inicio',component: NavebarComponent}
+    {path:'inicio',component: NavebarComponent},
+    {path:'tienda', component: TiendaComponent}
 ];
 @NgModule({
     imports:
